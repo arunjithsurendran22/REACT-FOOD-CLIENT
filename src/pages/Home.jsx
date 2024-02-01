6b4be119e4cf52f0018b5fd9cf6881df94468ae8
Arunjith Surendran <arunjithsurendran22@gmail.com>
2024-02-01 18:33:10 +0530

    cpu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Categories } from "../components/shared/Categories";
 import { HeroImage } from "../components/shared/HeroImage";
-import Restaurant from "../components/shared/Restaurant";
+// import Restaurant from "../components/shared/Restaurant";
 import Products from "../components/shared/Products";
 import burgerImage from "../assets/stickyBurgerhalf.png";
 import "../components/shared/Style/style.css";
@@ -24,12 +24,12 @@ const Home = () => {
       <div className="flex justify-center my-10">
         <Categories />
       </div>
-      <div className="flex justify-center  bg-gray-300 py-14">
+      {/* <div className="flex justify-center  bg-gray-300 py-14">
         <h1 className="text-4xl font-bold">Popular Restaurant</h1>
-      </div>
-      <div className="flex justify-center mt-10">
+      </div> */}
+      {/* <div className="flex justify-center mt-10">
         <Restaurant />
-      </div>
+      </div> */}
       <div className="flex justify-center">
         <Products />
       </div>
